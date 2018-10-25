ee6efecdb506fa71904d6bb46c13c0d9e8d0c6fa
masml <contact.masml@gmail.com>
2018-10-25 16:00:33 +0530

    trying opencv

--- application.py
+++ application.py
@@ -1,6 +1,34 @@
-from flask import Flask
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+
+# emulated camera
+from camera import Camera
+
+# If you are using a webcam -> no need for changes
+# if you are using the Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello Codefun!"
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + str(frame) + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
